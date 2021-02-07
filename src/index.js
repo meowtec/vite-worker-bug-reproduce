4a9eebeedf27b4651087b5cbacf63290788ec54d
meowtec <bertonzh@gmail.com>
2021-02-07 16:00:41 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as monaco from 'monaco-editor';
+import JSONWorker from './worker/json?worker&inline';
+import EditorWorker from './worker/editor?worker&inline';
+
+console.log(monaco, JSONWorker, EditorWorker);

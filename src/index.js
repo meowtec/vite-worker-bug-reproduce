dee31c5cea06e624e85b1490c589a6dd8058b56b
meowtec <bertonzh@gmail.com>
2021-02-07 16:28:49 +0800

    feat: add full example that monaco can work

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import * as monaco from 'monaco-editor';
 import JSONWorker from './worker/json?worker&inline';
 import EditorWorker from './worker/editor?worker&inline';
 
-console.log(monaco, JSONWorker, EditorWorker);
+const MonacoEnvironment = {
+  getWorker(workerId, label) {
+    if (label === 'json') {
+      return new JSONWorker();
+    }
+
+    return new EditorWorker();
+  },
+};
+
+window.MonacoEnvironment = MonacoEnvironment;
+
+const root = document.getElementById('app');
+
+monaco.editor.create(root, {
+  model: monaco.editor.createModel('', 'json'),
+});
